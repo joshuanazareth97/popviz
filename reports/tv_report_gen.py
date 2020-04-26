9f3e14649593402eae711549d026786eafbaade8
Joshua <joshuanazareth97@gmail.com>
2020-04-26 06:41:24 +0530

    Change layout

--- reports/tv_report_gen.py
+++ reports/tv_report_gen.py
@@ -6,8 +6,8 @@ from matplotlib import gridspec, offsetbox, pyplot as plt
 
 from reports.utils import pad_nan, wrap_text
 
-class TVReport:
 
+class TVReport:
     def __init__(self, data_provider):
         sns.set(font_scale=0.7)
         self.data = data_provider.seasons
@@ -33,40 +33,73 @@ class TVReport:
 
     @property
     def is_square(self):
-        return 1 <=  max(self.ratings.shape)/min(self.ratings.shape) < 1.3 
+        return 1 <= max(self.ratings.shape) / min(self.ratings.shape) < 1.3
 
     def _get_2d_array(self):
         ratings = []
         for season in self.data:
-            episodes = [np.float(episode["rating"]) for episode in season["episodes"] if episode["rating"]]
+            episodes = [
+                np.float(episode["rating"])
+                for episode in season["episodes"]
+                if episode["rating"]
+            ]
             ratings.append(episodes)
         return pad_nan(np.array(ratings))
 
     def _setup_page_layout(self, size="A4"):
-        size_map = {
-            "A4": (11.69, 8.27), # (width, height)
-            "A3": (16.53, 11.69)
-        }
+        size_map = {"A4": (11.69, 8.27), "A3": (16.53, 11.69)}  # (width, height)
         assert size in size_map
         page_width, page_height = size_map[size]
-        relative_heights = [1,5,3]
-        status = "Not Square"
-        optional_params = dict(xy=(0.5, 0.5), 
-            xycoords='axes fraction',
-            va='center', 
-            ha='center')
-        if self.is_square: 
-            status = "Square"
-            # relative_heights = [1,3,5]
-            page_height, page_width = page_width, page_height
+        relative_heights = [1, 5, 2, 2]
 
-        fig = plt.figure(figsize=(page_width, page_height), dpi=300, constrained_layout=True)
+        title_params = dict(
+            xy=(0.5, 0.7), xycoords="axes fraction", va="center", ha="center", size=32
+        )
+
+        subtitle_params = dict(
+            xy=(0.5, 0.2), xycoords="axes fraction", va="center", ha="center", size=14
+        )
+
+        if self.is_square:
+            # Change orientation to portrait
+            page_height, page_width = page_width, page_height
+            title_params["size"] = 32
+            relative_heights = [1, 5, 1, 2]
+
+        fig = plt.figure(
+            figsize=(page_width, page_height), dpi=300, constrained_layout=True
+        )
         fig.set_size_inches(page_width, page_height)
-        spec = gridspec.GridSpec(figure=fig, ncols=1, nrows=3, height_ratios=relative_heights)
-        title = fig.add_subplot(spec[0, :]).annotate(f"{self.show_metadata['title']}", **optional_params)
-        info = fig.add_subplot(spec[2, :]).annotate('Info', **optional_params)
+        spec = gridspec.GridSpec(
+            figure=fig, ncols=1, nrows=4, height_ratios=relative_heights
+        )
+        ep_info = gridspec.GridSpecFromSubplotSpec(1, 5, subplot_spec=spec[3, :])
+
+        with sns.axes_style("dark"):
+
+            title_ax = fig.add_subplot(spec[0, :])
+            show_info_ax = fig.add_subplot(spec[2, :])
+            misc = fig.add_subplot(ep_info[:, 2])
+            best_ep_ax = fig.add_subplot(ep_info[:, :2])
+            worst_ep_ax = fig.add_subplot(ep_info[:, 3:])
+            for ax in [title_ax, best_ep_ax, worst_ep_ax, show_info_ax, misc]:
+                sns.despine(ax=ax, bottom=True, left=True)  # , top=True, right=True)
+                ax.axes.xaxis.set_ticks([])
+                ax.axes.yaxis.set_ticks([])
+
+        title_ax.annotate(f"{self.show_metadata['title']}", **title_params)
+        running_date = self.show_metadata.get("running_date", "").replace(
+            "TV Series ", ""
+        )
+        title_ax.annotate(running_date, **subtitle_params)
+        self._fill_episode_info(best_ep_ax, cat="best")
+        self._fill_episode_info(worst_ep_ax, cat="worst")
         self.page = {
             "fig": fig,
+            "axes": [title_ax, best_ep_ax, worst_ep_ax],
+            "gridspec": spec,
+        }
+
     def _fill_episode_info(self, ax, cat="best"):
         best = cat == "best"
         info_params = dict(
@@ -107,7 +140,7 @@ class TVReport:
             {
                 # "ha": "left" if cat == "best" else "right",
                 "size": 12,
-        }
+            }
         )
         vertical_dist = 0.15 if self.is_square else 0.25
         for ep in ep_list:
@@ -157,7 +190,7 @@ class TVReport:
         # Setting up matplotlib and seaborn
         self._setup_page_layout()
         fig = self.page["fig"]
-        
+
         # Set up heatmap specific layout
         title_ax, best_ep_ax, worst_ep_ax = self.page["axes"]
 
@@ -206,7 +239,7 @@ class TVReport:
         average_opts.update(
             {"yticklabels": False, "xticklabels": ["Average"], "ax": average_ax}
         )
-        if self.inverted: 
+        if self.inverted:
             y_label, x_label = x_label, y_label
             average_opts.update(
                 {"yticklabels": ["Average"], "xticklabels": False,}
