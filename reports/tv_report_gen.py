7a3a4dc80e5de4fc5d52169c2092f0b9bdbd86c2
Joshua <joshuanazareth97@gmail.com>
2020-04-26 06:41:03 +0530

    Format according to black

--- reports/tv_report_gen.py
+++ reports/tv_report_gen.py
@@ -146,13 +146,13 @@ class TVReport:
 
     def heatmap(self, color="red", filename=None):
         colormap = {
-            'red': sns.color_palette("YlOrRd", 10),
-            'blue': sns.color_palette("YlGnBu", 10),
+            "red": sns.color_palette("YlOrRd", 10),
+            "blue": sns.color_palette("YlGnBu", 10),
         }
         height, width = self.ratings.shape
-        yticks = np.arange(1, height+1)
+        yticks = np.arange(1, height + 1)
         y_label = "Season"
-        xticks = np.arange(1, width+1)
+        xticks = np.arange(1, width + 1)
         x_label = "Episode"
         # Setting up matplotlib and seaborn
         self._setup_page_layout()
@@ -162,24 +162,36 @@ class TVReport:
         title_ax, best_ep_ax, worst_ep_ax = self.page["axes"]
 
         if self.inverted:
-            main_section = gridspec.GridSpecFromSubplotSpec(2,2, height_ratios=[height, 1], width_ratios=[width, 0.5], subplot_spec=self.page["gridspec"][1, :])
-            main_ax = fig.add_subplot(main_section[0,0])
-            average_ax = fig.add_subplot(main_section[1,0])
-            cbar_ax = fig.add_subplot(main_section[:,])
+            main_section = gridspec.GridSpecFromSubplotSpec(
+                2,
+                2,
+                height_ratios=[height, 1],
+                width_ratios=[width, 0.1],
+                subplot_spec=self.page["gridspec"][1, :],
+            )
+            main_ax = fig.add_subplot(main_section[0, 0])
+            average_ax = fig.add_subplot(main_section[1, 0])
+            cbar_ax = fig.add_subplot(main_section[:, 1])
         else:
-            main_section = gridspec.GridSpecFromSubplotSpec(1,3, width_ratios=[width, 1, 0.5], subplot_spec=self.page["gridspec"][1, :])
-            main_ax = fig.add_subplot(main_section[:,0])
-            average_ax = fig.add_subplot(main_section[:,1])
-            cbar_ax = fig.add_subplot(main_section[:,2])
+            main_section = gridspec.GridSpecFromSubplotSpec(
+                1,
+                3,
+                width_ratios=[width, 1, 0.5],
+                subplot_spec=self.page["gridspec"][1, :],
+            )
+            main_ax = fig.add_subplot(main_section[:, 0])
+            average_ax = fig.add_subplot(main_section[:, 1])
+            cbar_ax = fig.add_subplot(main_section[:, 2])
 
-
-        main_ax.xaxis.set_ticks_position('top')
-        main_ax.xaxis.set_label_position('top')
-        average_ax.xaxis.set_ticks_position('top')
+        main_ax.xaxis.set_ticks_position("top")
+        main_ax.xaxis.set_label_position("top")
+        average_ax.xaxis.set_ticks_position("top")
         opts = {
-            "vmax": 10,#min(10, median + 3),
-            "vmin": math.floor(np.nanmin(self.ratings)),#max(0, median - 3),
-            "cmap": colormap[color],#sns.cubehelix_palette(8, start=2, rot=0, dark=0, light=.95, reverse=True, as_cmap=True),#sns.color_palette("cubehelix_r", 10),
+            "vmax": 10,  # min(10, median + 3),
+            "vmin": math.floor(np.nanmin(self.ratings)),  # max(0, median - 3),
+            "cmap": colormap[
+                color
+            ],  # sns.cubehelix_palette(8, start=2, rot=0, dark=0, light=.95, reverse=True, as_cmap=True),#sns.color_palette("cubehelix_r", 10),
             "mask": (self.ratings == 0),
             "annot": True,
             "square": True,
@@ -191,25 +203,27 @@ class TVReport:
             # "cbar_ax": cbar
         }
         average_opts = opts.copy()
-        average_opts.update({
-                "yticklabels": False,
-                "xticklabels": ["Average"],
-                "ax": average_ax
-            })
+        average_opts.update(
+            {"yticklabels": False, "xticklabels": ["Average"], "ax": average_ax}
+        )
         if self.inverted: 
             y_label, x_label = x_label, y_label
-            average_opts.update({
-                "yticklabels": ["Average"],
-                "xticklabels": False,
-            })
+            average_opts.update(
+                {"yticklabels": ["Average"], "xticklabels": False,}
+            )
 
         average_opts.pop("mask")
-        sns.heatmap(self.ratings, **opts);
+        sns.heatmap(self.ratings, **opts)
         sns.heatmap(self.season_averages, **average_opts)
         sm = plt.cm.ScalarMappable(cmap="YlOrRd", norm=plt.Normalize(vmin=0, vmax=1))
         fig.colorbar(main_ax.collections[0], cax=cbar_ax)
         main_ax.set_xlabel(x_label)
         main_ax.set_ylabel(y_label)
         # plt.show()
+        if filename is None:
+            filename = self.show_metadata["title"].replace(" ", "_").lower()
+        fig.savefig(
+            f"./data/{filename}.png", dpi=300, bbox_inches="tight", pad_inches=0.2
+        )
         plt.close(fig)
         print("Heatmap generated, and saved to file.")
