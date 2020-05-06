2054430b2fc17a513cb881fe0c6461a91afd0ef9
Joshua <joshuanazareth97@gmail.com>
2020-05-06 07:05:38 +0530

    Add function for CLI

--- cli/__main__.py
+++ cli/__main__.py
@@ -1,15 +1,49 @@
-from tqdm import tqdm
+import sys
 
 from scraper import IMDBScraper
+from search import search_imdb
 from reports import TVReport
 
+
 def main():
-    show_data = []
-    iasip = IMDBScraper("tt0944947")
-    for season in tqdm(iasip.get_all_seasons(), desc="Retrieving data season-wise", total=iasip.latest_season):
-        show_data.append(season)
-    reporter = TVReport(data=show_data)
-    
+    """
+    Scrape the data for It's Always Sunny in Philadelphia
+    """
+    q = input("Enter a search term for a television show > ")
+    results = search_imdb(q)[:10]
+    if not results:
+        print("No results found! Check the search term.")
+        sys.exit(1)
+
+    for num, result in enumerate(results):
+        name = result["showname"]
+        cat = result["category"]
+        date = result["year"]
+        print(f"{num+1}. {name}\t({date})\t[{cat}]")
+    print()
+    while True:
+        choice = input("Choose one of the above shows > ")
+        try:
+            choice = int(choice) - 1
+        except ValueError:
+            print("Please enter a valid (numerical) choice.\n")
+            continue
+        try:
+            chosen = results[choice]
+        except IndexError:
+            print("Please enter a number present in the list above.")
+            continue
+        break
+
+    print(f"You have chosen {chosen['showname']} with ID: {chosen['id']}\n")
+    print("Retrieving data...")
+    scraper = IMDBScraper(chosen["id"])
+    reporter = TVReport(data_provider=scraper)
+    print("Generating report...")
+    reporter.heatmap()
+    reporter.save_file(filename="test", output_dir="./data")
+    print("Report saved.")
+
 
 if __name__ == "__main__":
     main()
