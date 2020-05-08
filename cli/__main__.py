987b81cf97c05feca2a87b07564e7c655c71823b
Joshua <joshuanazareth97@gmail.com>
2020-05-08 14:28:12 +0530

    Add basic CLI functionality

--- cli/__main__.py
+++ cli/__main__.py
@@ -1,48 +1,79 @@
 import sys
+import argparse
 
 from scraper import IMDBScraper
 from search import search_imdb
 from reports import TVReport
 
 
-def main():
-    """
-    Scrape the data for It's Always Sunny in Philadelphia
-    """
-    q = input("Enter a search term for a television show > ")
-    results = search_imdb(q)[:10]
+def get_results_from_imdb(query):
+    results = search_imdb(query)[:10]
     if not results:
         print("No results found! Check the search term.")
         sys.exit(1)
+    elif len(results) == 1:
+        chosen = results[0]
+    else:
+        for num, result in enumerate(results):
+            name = result["showname"]
+            cat = result["category"]
+            date = result["year"]
+            print(f"{num+1}. {name}\t({date})\t[{cat}]")
+        print()
+        while True:
+            choice = input("Choose one of the above shows > ")
+            try:
+                choice = int(choice) - 1
+            except ValueError:
+                print("Please enter a valid (numerical) choice.\n")
+                continue
+            if choice == -1:  # User input 0, which gets saved as -1
+                print("Exiting app. Bye bye!")
+                sys.exit(0)
+            try:
+                chosen = results[choice]
+            except IndexError:
+                print("Please enter a number present in the list above.")
+                continue
+            break
+        print(f"You have chosen {chosen['showname']} with ID: {chosen['id']}\n")
+    return chosen
 
-    for num, result in enumerate(results):
-        name = result["showname"]
-        cat = result["category"]
-        date = result["year"]
-        print(f"{num+1}. {name}\t({date})\t[{cat}]")
+
+def main():
     print()
-    while True:
-        choice = input("Choose one of the above shows > ")
-        try:
-            choice = int(choice) - 1
-        except ValueError:
-            print("Please enter a valid (numerical) choice.\n")
-            continue
-        try:
-            chosen = results[choice]
-        except IndexError:
-            print("Please enter a number present in the list above.")
-            continue
-        break
-
-    print(f"You have chosen {chosen['showname']} with ID: {chosen['id']}\n")
-    print("Retrieving data...")
-    scraper = IMDBScraper(chosen["id"])
+    parser = argparse.ArgumentParser(
+        description="Generate beautiful reports of IMDb ratings data."
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="Specify a filename for the output. Defaults to the name of the show.",
+    )
+    input_term_group = parser.add_mutually_exclusive_group()
+    input_term_group.add_argument(
+        "-s", "--search", help="Search for a television show."
+    )
+    input_term_group.add_argument(
+        "-i", "--id", help="Directly provide the IMDb ID of a television show."
+    )
+    args = parser.parse_args()
+    if not args.id:
+        if not args.search:
+            query = input("Enter a search term for a television show > ")
+        else:
+            query = args.search.strip()
+            print(f'Searching for "{query}" on IMDb...')
+        chosen = get_results_from_imdb(query)
+        chosen_id = chosen["id"]
+    else:
+        chosen_id = args.id
+    scraper = IMDBScraper(chosen_id)
     reporter = TVReport(data_provider=scraper)
     print("Generating report...")
     reporter.heatmap()
-    reporter.save_file(filename="test", output_dir="./data")
-    print("Report saved.")
+    file = reporter.save_file(output_dir="./data")
+    print(f"Report saved to {file}.")
 
 
 if __name__ == "__main__":
